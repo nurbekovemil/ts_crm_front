5054c60ef2363b267474c4f2dd0a2df8093b0b09
kseitdepartment <kse.mainstance@gmail.com>
2021-09-06 18:17:42 +0600

    sdfhb

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import {i18n} from './store/i18n'
 import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
@@ -15,6 +16,7 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
+  i18n,
   vuetify,
   render: h => h(App)
 }).$mount('#app')
